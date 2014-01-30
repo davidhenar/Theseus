5ba8f2121befd7b6f42b2a6a49b925d2bac093f2
davidhenar <david.henar@gmail.com>
2014-01-30 22:56:02 +0100

    GPL license added

--- src/org/shadoware/theseus/Labyrinth.java
+++ src/org/shadoware/theseus/Labyrinth.java
@@ -1,3 +1,10 @@
+/**
+ * Theseus. Labyrinth builder library.
+ * 
+ * @copyright (C) 2014 David Henar Palacin.
+ * @license GNU General Public License version 3 or later, see license.txt
+ */
+
 package org.shadoware.theseus;
 
 public interface Labyrinth {
